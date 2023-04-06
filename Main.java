0e60d93da8e350462da80a49af933f15ad0f429f
allister3879 <220956@astanait.edu.kz>
2023-04-06 17:55:05 +0600

    implementation of func to find average from array

--- Main.java
+++ Main.java
@@ -5,16 +5,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter task: ");
         int task = sc.nextInt();
-        switch (task){
-            case 1:
+        switch (task) {
+            case 1 -> {
                 System.out.println("Enter inputs:");
                 int n = sc.nextInt();
                 int[] arr = new int[n];
-                for(int i = 0; i < n; i++){
+                for (int i = 0; i < n; i++) {
                     arr[i] = sc.nextInt();
                 }
-                System.out.println("Minimum from array: "+findMin(n, arr));
-                break;
+                System.out.println("Minimum from array: " + findMin(n, arr));
+            }
+            case 2 -> {
+                System.out.println("Enter inputs:");
+                int n2 = sc.nextInt();
+                int[] arr2 = new int[n2];
+                for (int i = 0; i < n2; i++) {
+                    arr2[i] = sc.nextInt();
+                }
+                System.out.println("Average from array: " + findAverage(n2, arr2));
+            }
         }
     }
 
@@ -25,5 +34,13 @@ public class Main {
             int min = findMin(n-1, arr);
             return Math.min(min, arr[n-1]);
         }
+    }
+
+    public static double findAverage(int n, int[] arr){
+        double average = 0;
+        for(int i = 0; i < n; i++){
+            average += arr[i];
+        }
+        return average/n;
     }
 }
