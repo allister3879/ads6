801dd5411588321b31afd203bc34504ed7a9abe6
allister3879 <220956@astanait.edu.kz>
2023-04-06 19:04:55 +0600

    implementation of func to check prime number

--- Main.java
+++ Main.java
@@ -3,26 +3,39 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Please enter task: ");
-        int task = sc.nextInt();
-        switch (task) {
-            case 1 -> {
-                System.out.println("Enter inputs:");
-                int n = sc.nextInt();
-                int[] arr = new int[n];
-                for (int i = 0; i < n; i++) {
-                    arr[i] = sc.nextInt();
+        boolean checkingLab = true;
+        while (checkingLab) {
+            System.out.print("---Please enter task--- ");
+            int task = sc.nextInt();
+            switch (task) {
+                case 0 ->{
+                    System.out.println("--- end ---");
+                    checkingLab = false;
                 }
-                System.out.println("Minimum from array: " + findMin(n, arr));
-            }
-            case 2 -> {
-                System.out.println("Enter inputs:");
-                int n2 = sc.nextInt();
-                int[] arr2 = new int[n2];
-                for (int i = 0; i < n2; i++) {
-                    arr2[i] = sc.nextInt();
+                case 1 -> {
+                    System.out.println("Enter inputs:");
+                    int n = sc.nextInt();
+                    int[] arr = new int[n];
+                    for (int i = 0; i < n; i++) {
+                        arr[i] = sc.nextInt();
+                    }
+                    System.out.println("Minimum from array: " + findMin(n, arr));
                 }
-                System.out.println("Average from array: " + findAverage(n2, arr2));
+                case 2 -> {
+                    System.out.println("Enter inputs:");
+                    int n2 = sc.nextInt();
+                    int[] arr2 = new int[n2];
+                    for (int i = 0; i < n2; i++) {
+                        arr2[i] = sc.nextInt();
+                    }
+                    System.out.println("Average from array: " + findAverage(n2, arr2));
+                }
+                case 3 -> {
+                    System.out.println("Enter inputs: ");
+                    String n3 = sc.next();
+                    double limit = Math.pow(10, n3.length());
+                    System.out.println("Number is " + checkIfPrime(Integer.parseInt(n3), limit));
+                }
             }
         }
     }
@@ -35,12 +48,24 @@ public class Main {
             return Math.min(min, arr[n-1]);
         }
     }
-
     public static double findAverage(int n, int[] arr){
         double average = 0;
         for(int i = 0; i < n; i++){
             average += arr[i];
         }
         return average/n;
+    }
+    public static String checkIfPrime(int n, double limit){
+        int counter = 0;
+        for(int i = 1; i < limit; i++){
+            if(n%i==0){
+                counter += 1;
+            }
+        }
+        if(counter <= 2){
+            return "Prime";
+        }else {
+            return "Composite";
+        }
     }
 }
